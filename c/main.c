c36cf8c588c98520ed00a3f70324ad7d60d58944
yumzhi <2624187656@qq.com>
2021-07-30 11:15:39 +0800

    update the code style of main.c.

--- c/main.c
+++ c/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-	/* code */
-	printf("Hello world!\n");
-	
-	return 0;
+int main(int argc, char const *argv[]) {
+  /* code */
+  printf("Hello world!\n");
+
+  return 0;
 }
