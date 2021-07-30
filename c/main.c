17ccdceafe8b05de12b199d6af83b8b4776ccb3a
yumzhi <2624187656@qq.com>
2021-07-30 11:56:41 +0800

    update main.c.

--- c/main.c
+++ c/main.c
@@ -1,8 +1,20 @@
+/*
+ * code format:
+ *  clang-format -style=google -i main.c
+ * get configuration file:
+ *  clang-format -style=google -dump-config > .clang-format
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
   /* code */
+  int i = 0;
+
   printf("Hello world!\n");
 
+  for (i = 0; i < 10; ++i) {
+    /* code */
+    printf("%d\n", i);
+  }
   return 0;
 }
